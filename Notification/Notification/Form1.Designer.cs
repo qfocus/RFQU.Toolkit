e9f4d760dca5bda10303f9299e2005aeda9bbde4
rqu <ruifeng.qu@morningstar.com>
2020-09-23 16:51:54 +0800

    add new tool

---
+++ Notification/Notification/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Notification
+{
+    partial class Notification
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Notification));
+            this.Status = new System.Windows.Forms.GroupBox();
+            this.rbDisable = new System.Windows.Forms.RadioButton();
+            this.rbEnable = new System.Windows.Forms.RadioButton();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timePicker = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.notify = new System.Windows.Forms.NotifyIcon(this.components);
+            this.startsOnBoot = new System.Windows.Forms.CheckBox();
+            this.Status.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Status
+            // 
+            this.Status.Controls.Add(this.rbDisable);
+            this.Status.Controls.Add(this.rbEnable);
+            this.Status.Location = new System.Drawing.Point(25, 65);
+            this.Status.Name = "Status";
+            this.Status.Size = new System.Drawing.Size(156, 50);
+            this.Status.TabIndex = 0;
+            this.Status.TabStop = false;
+            this.Status.Text = "Status";
+            // 
+            // rbDisable
+            // 
+            this.rbDisable.AutoSize = true;
+            this.rbDisable.Location = new System.Drawing.Point(82, 20);
+            this.rbDisable.Name = "rbDisable";
+            this.rbDisable.Size = new System.Drawing.Size(60, 17);
+            this.rbDisable.TabIndex = 4;
+            this.rbDisable.Text = "Disable";
+            this.rbDisable.UseVisualStyleBackColor = true;
+            // 
+            // rbEnable
+            // 
+            this.rbEnable.AutoSize = true;
+            this.rbEnable.Checked = true;
+            this.rbEnable.Location = new System.Drawing.Point(18, 20);
+            this.rbEnable.Name = "rbEnable";
+            this.rbEnable.Size = new System.Drawing.Size(58, 17);
+            this.rbEnable.TabIndex = 3;
+            this.rbEnable.TabStop = true;
+            this.rbEnable.Text = "Enable";
+            this.rbEnable.UseVisualStyleBackColor = true;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(287, 92);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 5;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.Ok_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(368, 92);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 6;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.Cancel_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(79, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(364, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 999;
+            this.label1.Text = "Message";
+            // 
+            // timePicker
+            // 
+            this.timePicker.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.timePicker.Location = new System.Drawing.Point(79, 39);
+            this.timePicker.Name = "timePicker";
+            this.timePicker.ShowUpDown = true;
+            this.timePicker.Size = new System.Drawing.Size(102, 20);
+            this.timePicker.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 999;
+            this.label2.Text = "Time";
+            // 
+            // notify
+            // 
+            this.notify.Icon = ((System.Drawing.Icon)(resources.GetObject("notify.Icon")));
+            this.notify.Text = "Q";
+            this.notify.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notify_MouseDoubleClick);
+            // 
+            // startsOnBoot
+            // 
+            this.startsOnBoot.AutoSize = true;
+            this.startsOnBoot.Checked = true;
+            this.startsOnBoot.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.startsOnBoot.Location = new System.Drawing.Point(203, 39);
+            this.startsOnBoot.Name = "startsOnBoot";
+            this.startsOnBoot.Size = new System.Drawing.Size(164, 17);
+            this.startsOnBoot.TabIndex = 1000;
+            this.startsOnBoot.Text = "Starts when Windows startup";
+            this.startsOnBoot.UseVisualStyleBackColor = true;
+            // 
+            // Notification
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 129);
+            this.Controls.Add(this.startsOnBoot);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.timePicker);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.Status);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Notification";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Q";
+            this.Resize += new System.EventHandler(this.Notification_Resize);
+            this.Status.ResumeLayout(false);
+            this.Status.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox Status;
+        private System.Windows.Forms.RadioButton rbDisable;
+        private System.Windows.Forms.RadioButton rbEnable;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker timePicker;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NotifyIcon notify;
+        private System.Windows.Forms.CheckBox startsOnBoot;
+    }
+}
+
---
+++ Notification/Notification/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+namespace Notification
+{
+    public partial class Notification : Form
+    {
+        private readonly string REGISTER_PATH = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Run";
+        private readonly string REGISTER_KEY = "QRF_Notification";
+
+        private Timer alertTimer;
+        private DateTime date;
+
+        public Notification()
+        {
+            InitializeComponent();
+            LoadConfig();
+        }
+
+        private void Notification_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notify.Visible = true;
+            }
+        }
+
+        private void notify_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notify.Visible = false;
+        }
+
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            Hide();
+            notify.Visible = true;
+        }
+
+        private void Ok_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("Message is required!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                RegistryKey reg = Registry.CurrentUser.OpenSubKey(REGISTER_PATH, true);
+                if (startsOnBoot.Checked)
+                {
+                    reg.SetValue(REGISTER_KEY, Application.ExecutablePath.ToString());
+                }
+                else
+                {
+                    if (reg.GetValue(REGISTER_KEY, null) != null)
+                    {
+                        reg.DeleteValue(REGISTER_KEY);
+                    }
+
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("不好了！ 出错啦！\r\n" + ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (rbEnable.Checked)
+            {
+                if (alertTimer == null)
+                {
+                    alertTimer = new Timer();
+                    alertTimer.Interval = 1000;
+                    alertTimer.Tick += AlertTimer_Tick;
+                    alertTimer.Start();
+                }
+
+                date = DateTime.Now;
+            }
+            else
+            {
+                if (alertTimer != null)
+                {
+                    alertTimer.Stop();
+                }
+            }
+            SaveConfig();
+
+            Hide();
+            notify.Visible = true;
+        }
+
+        private void AlertTimer_Tick(object sender, EventArgs e)
+        {
+            if (date.Day != DateTime.Now.Day)
+            {
+                return;
+            }
+
+            if (timePicker.Value.TimeOfDay < DateTime.Now.TimeOfDay)
+            {
+                notify.ShowBalloonTip(1000, "404 友情提示", textBox1.Text, ToolTipIcon.Info);
+                date = DateTime.Now.AddDays(1);
+            }
+
+        }
+
+
+        private void LoadConfig()
+        {
+            Configuration configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var settings = configFile.AppSettings.Settings;
+
+            if (settings["Enable"] == null)
+            {
+                configFile.AppSettings.Settings.Add("Enable", "true");
+            }
+            else
+            {
+                rbEnable.Checked = Boolean.Parse(settings["Enable"].Value);
+                rbDisable.Checked = !rbEnable.Checked;
+            }
+
+            if (settings["StartsOnBoot"] == null)
+            {
+                settings.Add("StartsOnBoot", "true");
+            }
+            else
+            {
+                startsOnBoot.Checked = Boolean.Parse(settings["StartsOnBoot"].Value);
+            }
+
+            if (settings["Message"] == null || string.IsNullOrEmpty(settings["Message"].Value))
+            {
+                settings.Add("Message", "404 提醒你，是不是有什么事忘了做~");
+                textBox1.Text = "404 提醒你，是不是有什么事忘了做~";
+            }
+            else
+            {
+                textBox1.Text = settings["Message"].Value;
+            }
+
+            if (settings["AlertTime"] == null)
+            {
+                settings.Add("AlertTime", DateTime.Now.ToString());
+            }
+            else
+            {
+                DateTime dateTime;
+                if (DateTime.TryParse(settings["AlertTime"].Value, out dateTime))
+                {
+                    timePicker.Value = dateTime;
+                }
+
+            }
+
+            configFile.Save(ConfigurationSaveMode.Modified, true);
+        }
+
+
+        private void SaveConfig()
+        {
+            Configuration configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var setting = configFile.AppSettings.Settings;
+            setting["Enable"].Value = rbEnable.Checked.ToString();
+            setting["StartsOnBoot"].Value = startsOnBoot.Checked.ToString();
+            setting["Message"].Value = textBox1.Text.Trim();
+            setting["AlertTime"].Value = timePicker.Value.ToString();
+
+            configFile.Save(ConfigurationSaveMode.Modified, true);
+        }
+
+
+    }
+}
